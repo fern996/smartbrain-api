83730edeeb83f434e755f59bd98244beade7ff8e
sf0912 <shawnfernandes0912@gmail.com>
2022-12-09 13:52:19 -0500

    Updated databases

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.get('/', (req, res) => {res.send('it is working!')
-})
+app.get('/', (req, res) => {res.send('it is working!')})
 app.post('/signin', signin.handleSignin(db,bcrypt)) // Cleaner function call. More confusing. Might want  to adopt as standard.
 app.post('/register',(req,res) => {register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req,res)=> {profile.handleProfileGet(req,res, db)})
